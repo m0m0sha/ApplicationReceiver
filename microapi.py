6b83ad4fbdcc880266d89c8560309ec34707a8d5
m0m0sha <vova.bratsun@gmail.com>
2024-06-20 13:19:00 +0300

    Change get funcs

--- microapi.py
+++ microapi.py
@@ -1,4 +1,5 @@
 from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Session
 from models import ApplicationCreate, Application, SessionLocal, Base, engine
@@ -33,13 +34,15 @@ async def create_application(application: ApplicationCreate, db: AsyncSession = 
 
 @app.get("/applications", response_model=list[ApplicationCreate])
 async def get_applications(db: AsyncSession = Depends(get_db)):
-    applications = await db.query(Application).all()  # Получает все записи таблицы
+    result = await db.execute(select(Application))
+    applications = result.scalars().all() # Получает все записи таблицы
     return applications
 
 
 @app.get("/applications/{application_id}", response_model=ApplicationCreate)
 async def get_application(application_id: int, db: Session = Depends(get_db)):
-    application = await db.query(Application).filter(Application.id == application_id).first()
+    result = await db.execute(select(Application).filter(Application.id == application_id))
+    application = result.scalar_one_or_none()
     if application is None:
         raise HTTPException(status_code=404, detail="Application not found")
     return application
