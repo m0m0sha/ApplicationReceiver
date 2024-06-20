d95cfbd82835dc1aa2e80549433d8be7b9abb749
m0m0sha <vova.bratsun@gmail.com>
2024-06-20 15:23:50 +0300

    Add users and auth

--- main.py
+++ main.py
@@ -1,34 +1,63 @@
 import asyncio
+import aiohttp
 from microapi import app
 from models import Application, SessionLocal
 from telegram import Update
 from telegram.ext import Application as TelegramApplication, CommandHandler, ContextTypes
 
+from users import get_user
+
 TOKEN = '7378792011:AAFxT5ebUjpHLRmQkn1047vRpdtWkg1Ai0c'
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):  # Update содержит инфу о полученном сообщении
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Здравствуйте! Пожалуйста, оставьте вашу "
-                                                                          "заявку командой /apply <ваша заявка>")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text='Здравствуйте! Пожалуйста, оставьте вашу '
+                                                                          'заявку командой /apply <ваша заявка>')
 
 
 async def apply(update: Update, context: ContextTypes.DEFAULT_TYPE):
     application_text = ' '.join(context.args)
     if application_text:
         async with SessionLocal() as db:
+            db_user = await get_user(db, email=update.effective_user.email)
+            if not db_user:
+                await context.bot.send_message(chat_id=update.effective_chat.id, text='Для начала вам нужно '
+                                                                                      'зарегистрироваться! Используйте '
+                                                                                      'команду /register <email> '
+                                                                                      '<password> для регистрации')
+                return
             db_application = Application(application=application_text)
             db.add(db_application)
             await db.commit()
             await db.close()
-            await context.bot.send_message(chat_id=update.effective_chat.id, text=f'Ваша заявка "{application_text}" была '
-                                                                                  'принята!')
+            await context.bot.send_message(chat_id=update.effective_chat.id, text=f'Ваша заявка "{application_text}" '
+                                                                                  f'была принята!')
     else:
         await context.bot.send_message(chat_id=update.effective_chat.id, text='Пожалуйста, напишите текст заявки после '
                                                                               'команды /apply')
 
+
+async def register(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    args = context.args
+    if len(args) != 2:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text='Пожалуйста, используйте команду '
+                                                                              '/register <email> <password>')
+        return
+    email, password = args
+    url = f"http://m0m0sha-applicationreceiver-284d.twc1.net/users"
+    data = {"email": email, "password": password}
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, json=data) as response:
+            if response.status == 200:
+                await context.bot.send_message(chat_id=update.effective_chat.id, text='Вы успешно зарегистрировались!')
+            else:
+                await context.bot.send_message(chat_id=update.effective_chat.id, text='При регистрации произошла '
+                                                                                      'ошибка!')
+
 telegram_application = TelegramApplication.builder().token(TOKEN).build()  # Инициализация бота
 telegram_application.add_handler(CommandHandler("start", start))
 telegram_application.add_handler(CommandHandler("apply", apply))
+telegram_application.add_handler(CommandHandler("register", register))
 
 
 async def run_telegram_bot():
--- microapi.py
+++ microapi.py
@@ -1,15 +1,22 @@
+from datetime import timedelta
+
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Session
-from models import ApplicationCreate, Application, SessionLocal, Base, engine
+from starlette.status import HTTP_401_UNAUTHORIZED
+
+from models import ApplicationCreate, Application, SessionLocal, Base, engine, User, UserCreate, Token
+from users import get_user, get_password_hash, authenticate_user, ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token, \
+    get_current_active_user
 
 app = FastAPI()  # Создает приложения FastAPI
 
 
 async def init_db():
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all) # Создает таблицы из models
+        await conn.run_sync(Base.metadata.create_all)  # Создает таблицы из models
 
 
 @app.on_event("startup")
@@ -46,3 +53,37 @@ async def get_application(application_id: int, db: Session = Depends(get_db)):
     if application is None:
         raise HTTPException(status_code=404, detail="Application not found")
     return application
+
+
+@app.post("/users", response_model=User)
+async def create_user(user: UserCreate, db: AsyncSession = Depends(get_db)):
+    db_user = await get_user(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    hashed_password = get_password_hash(user.password)
+    db_user = User(email=user.email, hashed_password=hashed_password)
+    db.add(db_user)
+    await db.commit()
+    await db.refresh(db_user)
+    return db_user
+
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_db)):
+    user = await authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.email}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@app.get("/users/me", response_model=User)
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    return current_user
